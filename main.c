a2bdc76522cd30de5ab9cc6d1e8bca619068581d
Michal Tusnio <m.tusnio@gmail.com>
2016-11-24 15:35:38 +0000

    Awa fully integrated

--- main.c
+++ main.c
@@ -13,9 +13,54 @@
 
 #include "awa/static.h"
 
-void motion_callback(uint8_t event)
+#define COAP_PORT 6000
+#define BOOTSTRAP_SERVER_URI "coap://[fe80::219:f5ff:fe89:bfc]:15683"
+
+typedef struct
 {
+    int CarStatus;
+} ParkingSpot;
+
+
+static ParkingSpot spot;
+static AwaStaticClient * awaClient = NULL;
+
+static AwaStaticClient * InitialiseAwaClient()
+{
+    AwaStaticClient * client = AwaStaticClient_New();
+
+    AwaStaticClient_SetLogLevel(AwaLogLevel_Error);
+    AwaStaticClient_SetEndPointName(client, "Carpark1");
+    AwaStaticClient_SetCoAPListenAddressPort(client, "::", COAP_PORT);
+    AwaStaticClient_SetBootstrapServerURI(client, BOOTSTRAP_SERVER_URI);
+
+    AwaStaticClient_Init(client);
+
+    return client;
+}
+
+static void DefineObjects(AwaStaticClient * client)
+{
+    AwaStaticClient_DefineObject(client, 1337, "CarDetector", 0, 1);
+    AwaStaticClient_DefineResource(client, 1337, 1, "Temperature", AwaResourceType_Integer, 0, 1, AwaResourceOperations_ReadOnly);
+    AwaStaticClient_SetResourceStorageWithPointer(client, 1337, 1, &spot.CarStatus, sizeof(spot.CarStatus), sizeof(spot));
+}
+
+static void SetInitialValues(AwaStaticClient * client)
+{
+    AwaStaticClient_CreateObjectInstance(client, 1337, 0);
+    AwaStaticClient_CreateResource(client, 1337, 0, 1);
+    spot.CarStatus = 0;
+}
+
+
+
+static void motion_callback(uint8_t event)
+{
+    spot.CarStatus = (spot.CarStatus == 1) ? 0 : 1;
     leds_toggle(LED1);
+
+    AwaStaticClient_ResourceChanged(awaClient, 1337, 0, 1);
 }
 
 PROCESS(main_process, "Main process");
@@ -28,11 +73,23 @@ PROCESS_THREAD(main_process, ev, data)
     {
         PRINTF("=====Start=====\n");
 
+        awaClient = InitialiseAwaClient();
+
+        DefineObjects(awaClient);
+        SetInitialValues(awaClient);
+
         motion_click_enable(MIKROBUS_1);
+
+
+        static struct etimer et;
 
         motion_click_attach_callback(MIKROBUS_1, motion_callback);
         while(1)
         {
+            AwaStaticClient_Process(awaClient);
+
+            etimer_set(&et, CLOCK_SECOND / 2);
+            PROCESS_YIELD();
         }
 
         motion_click_disable(MIKROBUS_1);
