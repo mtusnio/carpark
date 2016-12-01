81cf25d0f48826e3ea4bdd179489eb7177e8d17b
Michal Tusnio <m.tusnio@gmail.com>
2016-12-01 14:37:32 +0000

    Updated code for bootstrap process

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 #include "awa/static.h"
 
 #define COAP_PORT 6000
-#define BOOTSTRAP_SERVER_URI "coap://[fe80::219:f5ff:fe89:bfc]:15683"
+#define BOOTSTRAP_SERVER_URI "coap://[" BOOTSTRAP_IPv6_ADDR "]:15683"
 
 typedef struct
 {
@@ -29,7 +29,7 @@ static AwaStaticClient * InitialiseAwaClient()
 {
     AwaStaticClient * client = AwaStaticClient_New();
 
-    AwaStaticClient_SetLogLevel(AwaLogLevel_Error);
+    AwaStaticClient_SetLogLevel(AwaLogLevel_Debug);
     AwaStaticClient_SetEndPointName(client, "Carpark1");
     AwaStaticClient_SetCoAPListenAddressPort(client, "::", COAP_PORT);
     AwaStaticClient_SetBootstrapServerURI(client, BOOTSTRAP_SERVER_URI);
@@ -42,7 +42,7 @@ static AwaStaticClient * InitialiseAwaClient()
 static void DefineObjects(AwaStaticClient * client)
 {
     AwaStaticClient_DefineObject(client, 1337, "CarDetector", 0, 1);
-    AwaStaticClient_DefineResource(client, 1337, 1, "Temperature", AwaResourceType_Integer, 0, 1, AwaResourceOperations_ReadOnly);
+    AwaStaticClient_DefineResource(client, 1337, 1, "Motion", AwaResourceType_Integer, 0, 1, AwaResourceOperations_ReadOnly);
     AwaStaticClient_SetResourceStorageWithPointer(client, 1337, 1, &spot.CarStatus, sizeof(spot.CarStatus), sizeof(spot));
 }
 
@@ -73,6 +73,15 @@ PROCESS_THREAD(main_process, ev, data)
     {
         PRINTF("=====Start=====\n");
 
+        uip_ipaddr_t ipaddr;
+        uip_ip6addr(&ipaddr, BOOTSTRAP_IPv6_ADDR1, BOOTSTRAP_IPv6_ADDR2, BOOTSTRAP_IPv6_ADDR3,
+            BOOTSTRAP_IPv6_ADDR4, BOOTSTRAP_IPv6_ADDR5, BOOTSTRAP_IPv6_ADDR6, BOOTSTRAP_IPv6_ADDR7,
+            BOOTSTRAP_IPv6_ADDR8);
+        uip_ds6_defrt_add(&ipaddr, 0);
+
+        uip_nameserver_update(&ipaddr, UIP_NAMESERVER_INFINITE_LIFETIME);
+
+
         awaClient = InitialiseAwaClient();
 
         DefineObjects(awaClient);
@@ -88,7 +97,7 @@ PROCESS_THREAD(main_process, ev, data)
         {
             AwaStaticClient_Process(awaClient);
 
-            etimer_set(&et, CLOCK_SECOND / 2);
+            etimer_set(&et, CLOCK_SECOND / 4);
             PROCESS_YIELD();
         }
 
