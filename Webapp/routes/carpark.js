7cb7ac49fda06e4ab6d2883bf58f877820637e6d
Michal Tusnio <m.tusnio@gmail.com>
2016-12-08 18:00:11 +0000

    Added status recovery for cars

--- Webapp/routes/carpark.js
+++ Webapp/routes/carpark.js
@@ -1,10 +1,18 @@
 var express = require('express');
-var exec = require('child_process').exec;
+var execSync = require('child_process').execSync;
+var StringDecoder = require('string_decoder').StringDecoder;
 var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('carpark', { title: 'Express' });
+    res.render('carpark', { title: 'Express' });
+});
+
+router.get("/car/:carId", function(req, res, next) {
+    carId = req.params.carId;
+    output = execSync("awa-server-read -c Carpark" + carId + " /1337/0/1");
+    decoder = new StringDecoder('utf-8');
+    res.json({ status: decoder.write(output).slice(-2)[0] });
 });
 
 module.exports = router;
