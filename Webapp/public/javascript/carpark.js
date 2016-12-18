315190b2f1739de16df25182664ebdbcd469fab5
Michal Tusnio <m.tusnio@gmail.com>
2016-12-18 21:57:40 +0000

    Updated carpark.js to query the whole range of cars

--- Webapp/public/javascript/carpark.js
+++ Webapp/public/javascript/carpark.js
@@ -1,15 +1,18 @@
+var CAR_COUNT=8
+
 function updateAllCars() {
-    for(i = 0; i < 6; i++) {
-        $.getJSON("/carpark/car/" + i, function( data ) {
-            carId = data.id;
-            if(data.status == 0) {
+    $.getJSON("/carpark/cars/1/" + CAR_COUNT, function(data) {
+        for(var i = 0; i < data.length; i++) {
+            carId = data[i].id;
+            if(data[i].status == 0) {
                 $("#car" + carId).css("background-color", "green");
             }
             else {
                 $("#car" + carId).css("background-color", "red");
             }
-        });
-    }
+
+        }
+    });
 }
 
 $( document ).ready(function() {
