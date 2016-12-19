c8282e8d0a6231225c28e7680813c8ebfd8b579c
Michal Tusnio <m.tusnio@gmail.com>
2016-12-19 01:11:51 +0000

    New graphics for the carpark added

--- Webapp/public/javascript/carpark.js
+++ Webapp/public/javascript/carpark.js
@@ -1,16 +1,31 @@
 var CAR_COUNT=8
+var PARKING_SPACE_IMAGE="parking_available.png"
+var CAR_IMAGES=5
+
+/**
+ * Returns a random integer between min (inclusive) and max (inclusive)
+ * Using Math.round() will give you a non-uniform distribution!
+ */
+function getRandomInt(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function getCarPicture(status) {
+    if(status == 1) {
+        return PARKING_SPACE_IMAGE;
+    }
+
+    return "car" + getRandomInt(1, CAR_IMAGES) + ".png";
+}
 
 function updateAllCars() {
     $.getJSON("/carpark/cars/1/" + CAR_COUNT, function(data) {
         for(var i = 0; i < data.length; i++) {
-            carId = data[i].id;
-            if(data[i].status == 0) {
-                $("#car" + carId).css("background-color", "green");
+            var carTag = "#car" + data[i].id;
+            if(data[i].status == 0 && !$(carTag).attr("src").includes(PARKING_SPACE_IMAGE)) {
+                continue;
             }
-            else {
-                $("#car" + carId).css("background-color", "red");
-            }
-
+            $(carTag).attr("src", "/images/carpark/" + getCarPicture(data[i].status));
         }
     });
 }
