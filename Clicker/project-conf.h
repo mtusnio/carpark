9355c8d4a1c43c5ce2e322b48175239e333a5842
Michal Tusnio <m.tusnio@gmail.com>
2017-01-05 14:37:25 +0000

    Clicker: Removed extra project_conf.h variables

--- Clicker/project-conf.h
+++ Clicker/project-conf.h
@@ -90,32 +90,6 @@
 
 #define UIP_CONF_BUFFER_SIZE           4096
 
-/* IP address of device */
-#define GLOBAL_IPv6_ADDR
-#define GLOBAL_IPv6_ADDR1              0xfd77
-#define GLOBAL_IPv6_ADDR2              0x9c74
-#define GLOBAL_IPv6_ADDR3              0xddee
-#define GLOBAL_IPv6_ADDR4              0xc0e7
-#define GLOBAL_IPv6_ADDR5              0x0
-#define GLOBAL_IPv6_ADDR6              0x0
-#define GLOBAL_IPv6_ADDR7              0x0
-#define GLOBAL_IPv6_ADDR8              0x0
-
-#define BOOTSTRAP_IPv6_ADDR1           GLOBAL_IPv6_ADDR1
-#define BOOTSTRAP_IPv6_ADDR2           GLOBAL_IPv6_ADDR2
-#define BOOTSTRAP_IPv6_ADDR3           GLOBAL_IPv6_ADDR3
-#define BOOTSTRAP_IPv6_ADDR4           GLOBAL_IPv6_ADDR4
-#define BOOTSTRAP_IPv6_ADDR5           0x0
-#define BOOTSTRAP_IPv6_ADDR6           0x0
-#define BOOTSTRAP_IPv6_ADDR7           0x0
-#define BOOTSTRAP_IPv6_ADDR8           0x1
-
 #define BOOTSTRAP_IPv6_ADDR            "fd77:9c74:ddee:c0e7::1"
-
-/* Enable button press simulation */
-/*
-   #define BUTTON_PRESS_SIMULATION
-   #define BUTTON_PRESS_PERIOD 5
- */
 
 #endif /* __PROJECT_LWM2M_CONF_H__ */
